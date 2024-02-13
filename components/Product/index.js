162a9efd6505d0e8076779cb1cab4306e4c0f916
Jan-Philipp Winkler <janphilipp.winkler@gmail.com>
2024-02-13 11:40:25 +0100

    first commit

---
+++ components/Product/index.js
@@ -0,0 +1,81 @@
+import useSWR from "swr";
+import { useRouter } from "next/router";
+import { ProductCard } from "./Product.styled";
+import { StyledLink } from "../Link/Link.styled";
+import Comments from "../Comments";
+import { useState } from "react";
+import { StyledButton } from "../Button/Button.styled";
+import ProductForm from "../ProductForm";
+
+export default function Product() {
+  async function handleEditProduct(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+    if (response.ok) {
+      mutate();
+    }
+  }
+
+  async function handleDeleteProduct(id) {
+    const response = await fetch(`/api/products/${id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      router.push("/");
+    }
+  }
+
+  const [isEditMode, setIsEditMode] = useState(false);
+
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!data) {
+    return;
+  }
+
+  return (
+    <ProductCard>
+      <h2>{data.name}</h2>
+      <p>Description: {data.description}</p>
+      <p>
+        Price: {data.price} {data.currency}
+      </p>
+      {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
+      {isEditMode && (
+        <ProductForm
+          value={data}
+          onSubmit={handleEditProduct}
+          isEditMode={isEditMode}
+        />
+      )}
+      <StyledLink href="/">Back to all</StyledLink>
+      <StyledButton
+        type="button"
+        onClick={() => {
+          setIsEditMode(!isEditMode);
+        }}
+      >
+        Edit
+      </StyledButton>
+      <StyledButton type="button" onClick={() => handleDeleteProduct(id)}>
+        Delete
+      </StyledButton>
+    </ProductCard>
+  );
+}
---
+++ components/ProductForm/index.js
@@ -0,0 +1,42 @@
+import { StyledForm, StyledHeading, StyledLabel } from "./ProductForm.styled";
+import { StyledButton } from "../Button/Button.styled";
+
+export default function ProductForm({ onSubmit, isEditMode, value }) {
+  return (
+    <StyledForm onSubmit={onSubmit}>
+      {!isEditMode && <StyledHeading>Add a new Fish</StyledHeading>}
+      <StyledLabel htmlFor="name">
+        Name:
+        <input type="text" id="name" name="name" defaultValue={value.name} />
+      </StyledLabel>
+      <StyledLabel htmlFor="description">
+        Description:
+        <input
+          type="text"
+          id="description"
+          name="description"
+          defaultValue={value.description}
+        />
+      </StyledLabel>
+      <StyledLabel htmlFor="price">
+        Price:
+        <input
+          type="number"
+          id="price"
+          name="price"
+          min="0"
+          defaultValue={value.price}
+        />
+      </StyledLabel>
+      <StyledLabel htmlFor="currency">
+        Currency:
+        <select id="currency" name="currency" defaultValue={value.currency}>
+          <option value="EUR">EUR</option>
+          <option value="USD">USD</option>
+          <option value="GBP">GBP</option>
+        </select>
+      </StyledLabel>
+      <StyledButton type="submit">Submit</StyledButton>
+    </StyledForm>
+  );
+}
